fc34bdc56afbe21c8116d83adfd7c47453c91805
asherliu <hliu77@stevens.edu>
2020-09-08 11:15:48 -0400

    mat_mult_mat draft source

---
+++ lec3/mat_mult_mat.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <assert.h>
+#include <stdlib.h>
+
+
+#ifndef __TIME_H__
+#define __TIME_H__
+
+#include <sys/time.h>
+#include <stdlib.h>
+
+double wtime()
+{
+	double time[2];
+	struct timeval time1;
+	gettimeofday(&time1, NULL);
+
+	time[0]=time1.tv_sec;
+	time[1]=time1.tv_usec;
+
+	return time[0]+time[1]*1.0e-6;
+}
+
+#endif
+
+int mat_mult_mat (int **A, int **B, int **res, int dim)
+{
+
+}
+
+
+int main(int args, char **argv)
+{
+    std::cout<<"Input: /path/to/exe dim\n";
+
+    assert(args == 2);
+
+    int dim = atoi(int argv[1]);
+
+    //Alloc the two dimensional arrays A, B and res;
+    //
+    //
+    //
+
+    //Random initialization of A and B.
+    //
+    //
+
+    double time = wtime();
+    mat_mul_mat (A, B, res, dim);
+    
+    time = wtime () - time;
+
+    std::cout<<"It takes " << time <<" seconds to finish the "<<dim<<" by "<<dim<< "matrix multiplication\n";
+
+    return 1;
+
+}
